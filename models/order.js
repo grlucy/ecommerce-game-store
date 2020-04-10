c0ac927cf0ab113ecd85fb446e57e9e50a715b8d
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-10 16:03:27 -0400

    added CartItem and Order schemas

---
+++ models/order.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
+
+const CartItemSchema = new mongoose.Schema({
+  product: { type: ObjectId, ref: "Product" },
+  name: String,
+  price: Number,
+  count: Number,
+});
+
+const CartItem = mongoose.model("CartItem", CartItemSchema);
+
+const OrderSchema = new mongoose.Schema(
+  {
+    products: [CartItemSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    status: {
+      type: String,
+      default: "Unfilled",
+      enum: ["Unfilled", "Filled", "Cancelled"],
+    },
+    updated: Date,
+    user: { type: ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = { Order, CartItem };
