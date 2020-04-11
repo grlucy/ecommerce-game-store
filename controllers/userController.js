3be04c2a01ef339d95fe9059e9a4e1381216f850
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-11 17:52:04 -0400

    added dbErrorHandler helper

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 const { User } = require("../models");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err,
+        err: errorHandler(err),
       });
     }
     res.json({
