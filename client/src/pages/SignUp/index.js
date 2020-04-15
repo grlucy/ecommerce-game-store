6f630f6999aff9466dd88d0f57d13a8c8a019474
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-15 16:47:59 -0600

    replaced SignUp and SignIn form components with a more generalized AuthForm component

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
-import SignUpForm from "../../components/SignUpForm";
+import AuthForm from "../../components/AuthForm";
+import InputField from "../../components/AuthForm/InputField";
+import SubmitBtn from "../../components/AuthForm/SubmitBtn";
 
 function SignUp() {
 
@@ -18,13 +20,68 @@ function SignUp() {
 
   const handleFormSubmit = event => {
     event.preventDefault();
+    API.signUp({
+      name: values.name,
+      email: values.email,
+      password: values.password
+    })
+    .then( data => {
+      if(data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true
+        });
+      }
+    })
+    .catch( err => {
+      setValues({ ...values, error: err.message, success: false });
+    });
   };
 
   return (
     <>
-      <SignUpForm onChange={handleInputChange} onSubmit={handleFormSubmit}/>
+      <AuthForm title="Sign Up">
+        <InputField
+          name="email"
+          label="Email Address:"
+          type="email"
+          placeholder="Email"
+          onChange={handleInputChange}
+          value={values.email}
+          icon="fas fa-envelope"
+          help=""
+        />
+        <InputField
+          name="name"
+          label="Name:"
+          type="text"
+          placeholder="Name"
+          onChange={handleInputChange}
+          value={values.name}
+          icon="fas fa-user"
+          help=""
+        />
+        <InputField
+          name="password"
+          label="Password:"
+          type="password"
+          placeholder="Password"
+          onChange={handleInputChange}
+          value={values.password}
+          icon="fas fa-lock"
+          help=""
+        />
+        <SubmitBtn 
+          onSubmit={handleFormSubmit}
+        />
+      </AuthForm>
       <div>
-        {/* {JSON.stringify(values)} */}
+        {JSON.stringify(values)}
       </div>
     </>
   );
