c481d4c6528ea3d62014341a95a008295e3828e1
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-17 10:51:51 -0600

    added signout method

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,6 +1,24 @@
+import API from "./API";
+
 export const authenticate = (data, next) => {
   if (typeof window !== "undefined") {
     localStorage.setItem("jwt", JSON.stringify(data));
     next();
+  }
+}
+
+export const signout = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+    return (
+      API.signOut()
+      .then((res) => {
+        console.log("sign out", res.data)
+      })
+      .catch((err) => {
+        console.log(err.response.data.error);
+      })
+    );
   }
 }
