a6e85eefc378dcfb92b3ee8564c59935468ef5fa
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-17 16:57:20 -0400

    added featured overlay to homepage

--- client/src/components/FeaturedCategory/index.js
+++ client/src/components/FeaturedCategory/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./featuredcategory.css";
 
 function FeaturedCategory(props) {
   return (
     <>
-      <div className="TBMarg LHeight">
+      <div className="TBMarg LHeight featured-container">
         <img className="align-bot featured-img" src={props.image} />
+        <div className="featured-overlay level is-mobile">
+          <div className="level-item has-text-centered">
+            <div>
+              <h1 className="title has-text-white">CLASSIC BOARD GAMES</h1>
+              <p className="has-text-white">
+                Now available in our online store
+              </p>
+              <p>
+                <Link
+                  to="/store"
+                  className="has-text-danger has-text-weight-bold"
+                >
+                  Shop Board Games ➤
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
