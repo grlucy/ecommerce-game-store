de3c84fc9e6d909155bcdadfb46fdfeeac0384d7
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-18 12:30:59 -0400

    added Hours component

--- client/src/components/Hours/index.js
+++ client/src/components/Hours/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function Hours() {
-
   return (
     <>
+      <h4 className="title is-4 has-text-black">Hours:</h4>
+      <p>M-F: 10:00 AM - 6:00 PM</p>
+      <p>Sat: 10:00AM - 10:00 PM</p>
+      <p>Sun: 11:00 AM - 4:00 PM</p>
     </>
   );
 }
