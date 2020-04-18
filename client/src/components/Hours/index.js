42fd19802e2cbac9fb7c656be0f68b91b5abf1db
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-18 13:16:28 -0400

    added ContactForm component

--- client/src/components/Hours/index.js
+++ client/src/components/Hours/index.js
@@ -4,9 +4,9 @@ function Hours() {
   return (
     <>
       <h4 className="title is-4 has-text-black">Hours:</h4>
-      <p>M-F: 10:00 AM - 6:00 PM</p>
-      <p>Sat: 10:00AM - 10:00 PM</p>
-      <p>Sun: 11:00 AM - 4:00 PM</p>
+      <p>M-F: 10 AM - 6 PM</p>
+      <p>Sat: 10 AM - 10 PM</p>
+      <p>Sun: 11 AM - 4 PM</p>
     </>
   );
 }
