363511a561bc9556d542d76fd14cc2d80aada537
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-18 12:24:56 -0400

    added address component

--- client/src/components/Address/index.js
+++ client/src/components/Address/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Address() {
-
   return (
     <>
+      <h4 className="title is-4 has-text-black">Address:</h4>
+      <p>410 Westhampton Way</p>
+      <p>Richmond, VA 23173</p>
     </>
   );
 }
