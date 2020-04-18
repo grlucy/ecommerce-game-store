b6ce848097e07da2ea4d6f9fc961803d9e7fee83
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-18 11:54:52 -0400

    set up react contact page

--- client/src/components/ContactSection/index.js
+++ client/src/components/ContactSection/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function ContactSection() {
+import FeatureImage from "../FeatureImage";
+import ContactInfo from "../ContactInfo";
+import ContactForm from "../ContactForm";
 
+function ContactSection() {
   return (
     <>
+      <div className="columns">
+        <div className="column is-one-quarter">
+          <FeatureImage />
+        </div>
+        <div className="column">
+          <ContactInfo />
+          <ContactForm />
+        </div>
+      </div>
     </>
   );
 }
