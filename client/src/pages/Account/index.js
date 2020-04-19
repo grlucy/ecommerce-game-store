f90964b4f3e60fa7e17a5cdfcf9da6a8ea42599f
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-19 00:00:17 -0600

    some initial work done on account page elements

--- client/src/pages/Account/index.js
+++ client/src/pages/Account/index.js
@@ -1,12 +1,48 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
+import { isAuthenticated } from "../../utils/auth";
+import "./style.css";
 
 function Account() {
 
+  const { user : { name, email }} = isAuthenticated();
+
   return (
-    <div>
-      Account Page
-    </div>
+    <section className="section">
+      <div className="container">
+        <div className="card account-card">
+          <header className="card-header">
+            <h3 className="card-header-title">User Information</h3>
+          </header>
+          <div className="card-content">
+            <ul>
+              <li>
+                <span className="icon">
+                  <i className="fas fa-user"></i>
+                </span>
+                {name}
+              </li>
+              <li>
+                <span className="icon">
+                  <i className="fas fa-envelope"></i>
+                </span>
+                {email}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card account-card">
+          <header className="card-header">
+            <h3 className="card-header-title">Order History</h3>
+          </header>
+          <div className="card-content">
+            <ul>
+              <li>history</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
