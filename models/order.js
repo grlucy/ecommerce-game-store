f28fa38daca84e724d73783f7cf83a4b5c58ca5a
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-21 12:38:48 -0400

    added pickup field to OrderSchema

--- models/order.js
+++ models/order.js
@@ -16,6 +16,7 @@ const OrderSchema = new mongoose.Schema(
     products: [CartItemSchema],
     transaction_id: {},
     amount: { type: Number },
+    pickup: String,
     status: {
       type: String,
       default: "Unfilled",
