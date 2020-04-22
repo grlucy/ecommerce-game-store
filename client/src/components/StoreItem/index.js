6212c05a8bf0581ae7626f830603d06d48f24007
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-22 01:08:52 -0600

    Add To Cart functionality is now implemented

--- client/src/components/StoreItem/index.js
+++ client/src/components/StoreItem/index.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import "./style.css";
+import { addItem } from "../../utils/cartHelpers";
 
 function StoreItem(props) {
 
   const { name, price, category, quantity, image, description} = props.product;
 
   const [purchaseQty, setPurchaseQty] = useState(quantity > 0 ? 1 : 0);
+  const [redirect, setRedirect] = useState(false);
 
   const handleQtyChange = (event) => {
     if (event.target.value >= 0) {
@@ -29,65 +32,70 @@ function StoreItem(props) {
   }
 
   const handleAddToCart = (event) => {
-    console.log("ADDING TO THAT CART!");
+    addItem(props.product, purchaseQty, () => {
+      setRedirect(true);
+    })
   }
 
   return (
-    <div className="container">
-      <div className="tile is-ancestor store-item">
-        <div className="tile is-parent is-4">
-          <div className="tile is-child">
-            <img className="product-img" src={image} alt={"Image of " + name}/>
+    <>
+      {redirect ? <Redirect to="/cart" /> : <></>}
+      <div className="container">
+        <div className="tile is-ancestor store-item">
+          <div className="tile is-parent is-4">
+            <div className="tile is-child">
+              <img className="product-img" src={image} alt={"Image of " + name}/>
+            </div>
           </div>
-        </div>
-        <div className="tile is-parent is-vertical is-8">
-          <div className="tile is-child">
-            <p className="title is-size-3">{name}</p>
-            <p className="subtitle is-size-6"><strong>Category: </strong>{category}</p>
-            <div className="level">
-              <div className="level-left">
-                <div className="level-item"><strong>Price: <span className="has-text-danger is-size-4">${price}</span></strong></div>
-              </div>
-              <div className="level-right">
-                <div className="level-item">
-                  <div className="field is-horizontal">
-                    <div className="field-label is-small quantity-label">
-                      <label className="label">Qty:</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <div className="control">
-                          {quantity > 0 ?
-                            <input 
-                              className="input is-radiusless is-small quantity-field"
-                              type="number"
-                              onChange={handleQtyChange}
-                              value={purchaseQty}
-                            />
-                            :
-                            <input 
-                              className="input is-radiusless is-small quantity-field"
-                              type="number"
-                              onChange={handleQtyChange}
-                              value={purchaseQty}
-                              disabled
-                            />
-                          }
+          <div className="tile is-parent is-vertical is-8">
+            <div className="tile is-child">
+              <p className="title is-size-3">{name}</p>
+              <p className="subtitle is-size-6"><strong>Category: </strong>{category}</p>
+              <div className="level">
+                <div className="level-left">
+                  <div className="level-item"><strong>Price: <span className="has-text-danger is-size-4">${price}</span></strong></div>
+                </div>
+                <div className="level-right">
+                  <div className="level-item">
+                    <div className="field is-horizontal">
+                      <div className="field-label is-small quantity-label">
+                        <label className="label">Qty:</label>
+                      </div>
+                      <div className="field-body">
+                        <div className="field">
+                          <div className="control">
+                            {quantity > 0 ?
+                              <input 
+                                className="input is-radiusless is-small quantity-field"
+                                type="number"
+                                onChange={handleQtyChange}
+                                value={purchaseQty}
+                              />
+                              :
+                              <input 
+                                className="input is-radiusless is-small quantity-field"
+                                type="number"
+                                onChange={handleQtyChange}
+                                value={purchaseQty}
+                                disabled
+                              />
+                            }
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                  {cartButton()}
                 </div>
-                {cartButton()}
               </div>
             </div>
-          </div>
-          <div className="tile is-child">
-            <p>{description}</p>
+            <div className="tile is-child">
+              <p>{description}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ client/src/utils/cartHelpers.js
@@ -0,0 +1,22 @@
+export const addItem = (item, qty, next) => {
+  let cart = [];
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+    cart.push({
+      ...item,
+      count: qty
+    });
+
+    //This code removes duplicate instances of an item from the cart
+    //new Set does not allow dup values, so we pass it the ids
+    //Then run map on the resulting array of ids to map back to actual product object
+    cart = Array.from(new Set(cart.map((p) => (p._id)))).map((id) => {
+      return cart.find(p => p._id === id);
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    next();
+  }
+}
