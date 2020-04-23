acee5bb821093ca3bc497c7b34d51e2426b71265
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-22 22:24:49 -0600

    added getCart function

--- client/src/utils/cartHelpers.js
+++ client/src/utils/cartHelpers.js
@@ -29,3 +29,12 @@ export const itemTotal = () => {
   }
   return 0;
 };
+
+export const getCart = () => {
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("cart")) {
+      return JSON.parse(localStorage.getItem("cart"));
+    }
+  }
+  return [];
+};
