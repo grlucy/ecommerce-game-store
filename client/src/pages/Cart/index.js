65c41a3c25a37183cd71b341186723a2a452a122
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-22 22:27:13 -0600

    some initial work done on building out items section of cart page

--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,10 +1,55 @@
 import React, { useState, useEffect } from "react";
-import API from "../../utils/API";
+import { getCart } from "../../utils/cartHelpers";
+import CartItem from "../../components/CartItem";
 
 function Cart() {
 
+  const [ items, setItems ] = useState([]);
+
+  useEffect(() => {
+    setItems(getCart());
+  }, []);
+
+  const handleCountChange = () => {
+    console.log("handling that count change");
+  }
+
+  const removeItemFromCart = (item) => {
+    console.log("removing that there item");
+  }
+
+  const renderEmpty = () => {
+    return <div>Hey howdy hey!</div>;
+  }
+
+  const renderItems = () => {
+    return items.map((item) => {
+      return (
+        <CartItem
+          item={item}
+          onCountChange={handleCountChange}
+          removeFromCart={removeItemFromCart}
+          key={item._id}
+        />
+      );
+    });
+  }
+
   return (
     <>
+      <div className="title page-title has-text-centered is-2">
+        CART
+      </div>
+      <section className="section">
+        <div className="columns">
+          <div className="column is-three-fifths">
+            { items.length === 0 ? renderEmpty() : renderItems() }
+          </div>
+          <div className="column is-two-fifths">
+            {/* render braintree checkout stuff here */}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
