8c5882306759f6dc42dae4c8bc88f058011b5e0b
unknown <53538316+grlucy@users.noreply.github.com>
2020-06-16 14:56:43 -0400

    updated image urls

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -12,7 +12,7 @@ const productSeed = [
     price: 29.99,
     category: "Board Games",
     quantity: 25,
-    image: "https://dragonsden12.s3.amazonaws.com/products/chess2.jpg",
+    image: "assets/images/stock-photos/products/chess2.jpg",
     description:
       "A classic wooden chess set in black and white. Pieces can be stored inside of hinged game board.",
     numberSold: 8,
@@ -22,7 +22,7 @@ const productSeed = [
     price: 56.99,
     category: "Board Games",
     quantity: 0,
-    image: "https://dragonsden12.s3.amazonaws.com/products/checkers.jpg",
+    image: "assets/images/stock-photos/products/checkers.jpg",
     description:
       "A giant black and white checkerboard perfect for your next party. Dimensions: 6' x 6'",
     numberSold: 5,
@@ -32,7 +32,7 @@ const productSeed = [
     price: 35.99,
     category: "Board Games",
     quantity: 10,
-    image: "https://dragonsden12.s3.amazonaws.com/products/mahjong.jpg",
+    image: "assets/images/stock-photos/products/mahjong.jpg",
     description:
       "A game of skill using a set of 144 tiles. Includes rulebook with five regional variations.",
     numberSold: 1,
@@ -42,7 +42,7 @@ const productSeed = [
     price: 32.99,
     category: "Puzzles",
     quantity: 10,
-    image: "https://dragonsden12.s3.amazonaws.com/products/skylinepuzzle.jpg",
+    image: "assets/images/stock-photos/products/skylinepuzzle.jpg",
     description:
       "A 1,000-piece puzzle that features a photograph of a cityscape.",
     numberSold: 12,
@@ -52,7 +52,7 @@ const productSeed = [
     price: 19.99,
     category: "Puzzles",
     quantity: 15,
-    image: "https://dragonsden12.s3.amazonaws.com/products/flowerpuzzle.jpg",
+    image: "assets/images/stock-photos/products/flowerpuzzle.jpg",
     description:
       "A 500-piece puzzle that features a photograph of white flowers.",
     numberSold: 2,
@@ -62,7 +62,7 @@ const productSeed = [
     price: 12.99,
     category: "Puzzles",
     quantity: 0,
-    image: "https://dragonsden12.s3.amazonaws.com/products/puppypuzzle.jpg",
+    image: "assets/images/stock-photos/products/puppypuzzle.jpg",
     description:
       "A 30-piece puzzle ideal for young children that features a photograph of a puppy.",
     numberSold: 25,
@@ -72,7 +72,7 @@ const productSeed = [
     price: 4.99,
     category: "Cards",
     quantity: 50,
-    image: "https://dragonsden12.s3.amazonaws.com/products/cards.jpg",
+    image: "assets/images/stock-photos/products/cards.jpg",
     description:
       "A standard French-suited deck of 52 playing cards. Includes jokers.",
     numberSold: 14,
@@ -92,7 +92,7 @@ const productSeed = [
     price: 28.99,
     category: "Cards",
     quantity: 40,
-    image: "https://dragonsden12.s3.amazonaws.com/products/tarot.jpg",
+    image: "assets/images/stock-photos/products/tarot.jpg",
     description:
       "A deck of 78 divinatory cards featuring the late 15th century art of the Sola Busca tarot.",
     numberSold: 10,
